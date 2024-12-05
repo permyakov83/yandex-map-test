28dacf645be1dd97bc682f2fa7c93d5110276657
permyakov83 <permyakov83@gmail.com>
2024-12-05 16:10:19 +0500

    error

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {
+  YMap,
+  YMapDefaultSchemeLayer,
+  YMapDefaultFeaturesLayer,
+  YMapMarker,
+  reactify
+} from './lib/ymap';
+import type { YMapLocationRequest } from 'ymaps3';
+
+const LOCATION: YMapLocationRequest = {
+  center: [37.58, 55.73],
+  zoom: 9
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ width: '600px', height: '400px' }}>
+      <YMap location={reactify.useDefault(LOCATION)}>
+        <YMapDefaultSchemeLayer />
+        <YMapDefaultFeaturesLayer />
+
+        <YMapMarker
+          coordinates={reactify.useDefault([37.588144, 55.733842])}
+          draggable={true}
+        >
+          <section>
+            <h1>You can drag this header</h1>
+          </section>
+        </YMapMarker>
+      </YMap>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/lib/ymap.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const [ymaps3React] = await Promise.all([
+  ymaps3.import('@yandex/ymaps3-reactify'),
+  ymaps3.ready
+]);
+
+export const reactify = ymaps3React.reactify.bindTo(React, ReactDom);
+export const {
+  YMap,
+  YMapDefaultSchemeLayer,
+  YMapDefaultFeaturesLayer,
+  YMapDefaultMarker,
+  YMapMarker
+} = reactify.module(ymaps3);
